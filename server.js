8e52ab76bbd7e3c4330bbaac10a903193107b17c
Antonio Galaz <antonio.galaz.aroca@gmail.com>
2019-03-07 22:20:58 -0300

    Primer Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        name: 'anTonio gaLaz',
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {});
+
+});
+
+app.listen(3000, () => {
+    console.log(`Escuchando en el puerto ${ port }`);
+});
